9e46f02b5b2615440c7fdd00aaf0c14bb1bc4cd1
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-10-02 16:11:41 +0530

    Covid-19

---
+++ src/components/CovidData.jsx
@@ -0,0 +1,112 @@
+// import React from 'react'
+// import "./CovidData.css"
+// export  function CovidData() {
+//   return (
+//     <div>
+//         <h1>Covid-19</h1>
+//         </div>
+//   )
+// }
+
+import React, { useEffect, useState } from "react";
+import "./CovidData.css";
+
+export function CovidData() {
+  const [country, setCountry] = useState("");
+  const [cases, setCases] = useState("");
+  const [recovered, setRecovered] = useState("");
+  const [deaths, setDeaths] = useState("");
+  const [todayCases, setTodayCases] = useState("");
+  const [deathCases, setDeathCases] = useState("");
+  const [recoveredCases, setRecoveredCases] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/countries")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  const setData = ({
+    country,
+    cases,
+    deaths,
+    recovered,
+    todayCases,
+    todayDeaths,
+    todayRecovered,
+  }) => {
+    setCountry(country);
+    setCases(cases);
+    setRecovered(recovered);
+    setDeaths(deaths);
+    setTodayCases(todayCases);
+    setDeathCases(todayDeaths);
+    setRecoveredCases(todayRecovered);
+  };
+
+  const handleSearch = (e) => {
+    setUserInput(e.target.value);
+    // covidData__country__info ="block"
+  };
+
+  // const handleDarkThemeChange = () => {
+  //     setDarkTheme(!darkTheme);
+  //   };
+
+  const handleSubmit = (props) => {
+    props.preventDefault();
+    fetch(`https://disease.sh/v3/covid-19/countries/${userInput}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  };
+
+  return (
+    <div className="covidData">
+      {/* <div>
+        backgroundColor={darkTheme ? "dark" : "light"}
+        text={darkTheme ? "light" : "dark"}
+        className="text-center"
+        style={{ margin: "10px" }}
+      </div>
+         */}
+
+      {/* style={{
+        backgroundColor: darkTheme ? "black" : "white",
+        color: darkTheme ? "white" : "black",
+      }} */}
+
+      <h1>COVID-19 CASES COUNTRY WISE</h1>
+      <div className="covidData__input">
+        <form onSubmit={handleSubmit}>
+          {/* input county name */}
+          <input onChange={handleSearch} placeholder="Enter Country Name" />
+          <br />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
+      {/* Showing the details of the country */}
+      <div className="covidData__country__info">
+        <p>Country Name : {country} </p>
+
+        <p>Cases : {cases}</p>
+
+        <p>Deaths : {deaths}</p>
+
+        <p>Recovered : {recovered}</p>
+
+        <p>Cases Today : {todayCases}</p>
+
+        <p>Deaths Today : {deathCases}</p>
+
+        <p>Recovered Today : {recoveredCases}</p>
+      </div>
+    </div>
+  );
+}
